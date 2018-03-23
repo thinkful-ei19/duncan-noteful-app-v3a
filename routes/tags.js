018971ffc4ca030eacf955806183b59eed9ffea0
trentduncan <trent.dunc@gmail.com>
2018-03-23 09:39:44 -0700

    finished all tags routes endpoints except delete

---
+++ routes/tags.js
@@ -0,0 +1,116 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+
+const Tag = require('../models/tag');
+
+/* ========== GET/READ ALL ITEMS ========== */
+router.get('/tags', (req, res, next) => {
+  Tag.find()
+    .sort('name')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(next);
+});
+    
+//   /* ========== GET/READ A SINGLE ITEM ========== */
+router.get('/tags/:id', (req, res, next) => {
+  const { id } = req.params;
+    
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+    
+  Tag.findById(id)
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(next);
+});
+    
+// //   /* ========== POST/CREATE AN ITEM ========== */
+router.post('/tags', (req, res, next) => {
+  const {name} = req.body;
+    
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+  const newItem = {name};
+  
+  Tag.create(newItem)
+    .then(result => {
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The tag name already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
+});
+    
+// //   /* ========== PUT/UPDATE A SINGLE ITEM ========== */
+router.put('/tags/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { name } = req.body;
+    
+  /***** Never trust users - validate input *****/
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+    
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+    
+  const updateItem = { name };
+  const options = { new: true };
+    
+  Tag.findByIdAndUpdate(id, updateItem, options)
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The tag name already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
+});
+    
+// //   /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
+router.delete('/tags/:id', (req, res, next) => {
+  const { id } = req.params;
+    
+  Tag.findByIdAndRemove(id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+module.exports = router;
--- scratch/queries.js
+++ scratch/queries.js
@@ -6,12 +6,17 @@ const { MONGODB_URI } = require('../config');
 
 const Note = require('../models/note');
 
-const Folder = require('../models/folder');
+// const Folder = require('../models/folder');
 
 mongoose.connect(MONGODB_URI)
-  .then(() => Folder.find())
-  .populate('Note')
-  .then(res => {
-    console.log(res);
-  });
+  .then(() => {
+    Note.update(
+      { },
+      { $pull: {tags: ['222222222222222222222201'] } },
+      { multi: true }
+    );
+  })
+  .then(res => console.log(res))
+  .then(() => mongoose.disconnect());
+
   
